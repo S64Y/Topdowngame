a788007d390e2ccab7fcf9de529280674912b262
S64Y <Saulmendez2567@gmail.com>
2023-12-05 13:52:04 -0500

    added new script

---
+++ TopDownGame/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int gems = 0;
+
+    [SerializeField] private Text gemsText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("GEM"))
+        {
+            Destroy(collision.gameObject);
+            gems++;
+            gemsText.text = "Gems: " + gems;
+        }
+
+    }
+
+
+
+
+}
